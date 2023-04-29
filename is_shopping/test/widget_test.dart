7e2c028f1c5ca32c4b7fd82c5082dbab8eecbd2a
Ákos Lovász <akos.lovasz@chameleon.sh>
2023-04-29 13:19:41 +0200

    remove test

--- is_shopping/test/widget_test.dart
+++ is_shopping/test/widget_test.dart
@@ -13,8 +13,6 @@ import 'package:is_shopping/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
