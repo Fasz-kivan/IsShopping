5262e0d4e174a5940adb2ae9f0c9534698cdf82e
Ákos Lovász <lovasz.akos.dev@gmail.com>
2023-07-09 19:06:11 +0200

    added count and date added fields to items

--- lib/shopping_item.dart
+++ lib/shopping_item.dart
@@ -2,7 +2,13 @@ class ShoppingItem {
   String itemName;
   String emoji;
   bool isChecked;
+  DateTime addedAt;
+  int? count;
 
   ShoppingItem(
-      {required this.itemName, required this.emoji, this.isChecked = false});
+      {required this.itemName,
+      required this.emoji,
+      this.isChecked = false,
+      required this.addedAt,
+      this.count});
 }
