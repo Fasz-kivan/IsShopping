3b50c86f1d27e21ac67e1b63b211edd838eaddb6
Lovász Ákos <akos.lovasz@chameleon.sh>
2025-09-14 18:35:58 +0200

    expand emoji regex

--- lib/main.dart
+++ lib/main.dart
@@ -35,13 +35,22 @@ class MainScreen extends State<MainScreenDisplayer> {
   Offset _longPressPosition = Offset.zero;
 
   static final RegExp emojiRegex = RegExp(
-    r'[\u{1F300}-\u{1F5FF}' // Miscellaneous Symbols and Pictographs
-    r'\u{1F600}-\u{1F64F}' // Emoticons
+    r'[\u{1F000}-\u{1F02F}' // Mahjong Tiles
+    r'\u{1F0A0}-\u{1F0FF}' // Playing Cards
+    r'\u{1F100}-\u{1F64F}' // Enclosed Alphanumeric Supplement + Emoticons
     r'\u{1F680}-\u{1F6FF}' // Transport and Map Symbols
+    r'\u{1F700}-\u{1F77F}' // Alchemical Symbols
+    r'\u{1F780}-\u{1F7FF}' // Geometric Shapes Extended
+    r'\u{1F800}-\u{1F8FF}' // Supplemental Arrows-C
+    r'\u{1F900}-\u{1F9FF}' // Supplemental Symbols and Pictographs
+    r'\u{1FA00}-\u{1FA6F}' // Chess Symbols
+    r'\u{1FA70}-\u{1FAFF}' // Symbols and Pictographs Extended-A
+    r'\u{1FB00}-\u{1FBFF}' // Symbols for Legacy Computing
+    r'\u{2190}-\u{21FF}' // Arrows
     r'\u{2600}-\u{26FF}' // Miscellaneous Symbols
     r'\u{2700}-\u{27BF}' // Dingbats
-    r'\u{1F900}-\u{1F9FF}' // Supplemental Symbols and Pictographs
-    r'\u{1F1E0}-\u{1F1FF}' // Flags (iOS)
+    r'\u{FE00}-\u{FE0F}' // Variation Selectors
+    r'\u{1F1E0}-\u{1F1FF}' // Flags
     r']',
     unicode: true,
   );
